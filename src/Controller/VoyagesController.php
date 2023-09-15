ea990a018299f75545da4d434e3f821c2c6d7dea
Azurly <azerly.hn@gmail.com>
2023-09-15 18:14:33 +0200

    commit

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -1,17 +1,28 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VoyagesController extends AbstractController{
-/**
- * @Route("/voyages", name="voyages")
- * @return Response
- */
+    
+    private $repository;
+
+    public function __construct(VisiteRepository $repository){
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/voyages", name="voyages")
+     * @return Response
+     */
     public function index() : Response{
-        return $this->render('pages/voyages.html.twig');
+        $visites = $this->repository->findAll();
+        return $this->render('pages/voyages.html.twig', [
+            'visites' => $visites
+        ]);
     }
 }
 ?>
